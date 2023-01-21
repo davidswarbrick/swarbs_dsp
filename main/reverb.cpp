9d4edec9c34b57bc27d1f7a25eb5dbc0d1da65dd
David Swarbrick <david.swarbrick9@gmail.com>
2023-01-21 18:24:28 +0000

    Add handwritten all-pass filter structure, plus normalise and smooth function

--- main/reverb.cpp
+++ main/reverb.cpp
@@ -1,40 +1,55 @@
-//https://github.com/Rishikeshdaoo/Reverberator/blob/master/Reverberator/src/com/rishi/reverb/Reverberation.java
-void comb_filter(float ** inSlot, 
-                 float ** outSlot,
+#include <algorithm>
+// Mono, could deconstruct float ** into two float * (outSlot[0], outSlot[1]) for stereo.
+void comb_filter(float * inSlot, 
+                 float * outSlot,
                  int buffer_size,
+                 int sample_rate,
                  float delay_ms,
-                 float decay_factor,
-                 int sample_rate)
+                 float gain)
 {
     // Calculate delay in samples from delay in ms and samples/second
     int delay_samples = (int) (delay_ms * (sample_rate/1000));
-    // ToDo: more efficient copy
-    for (int i = 0; i< buffer_size; i++){
-        outSlot[0][i] = inSlot[0][i];
-        outSlot[1][i] = inSlot[1][i];
+    for (int i = 0; i<delay_samples; i++){
+        // Output is zero until delay reached.
+        outSlot[i] = 0;
     }
-    for (int i = 0; i< buffer_size - delay_samples; i++){
-        outSlot[0][i+delay_samples] = outSlot[0][i]*decay_factor;
-        outSlot[1][i+delay_samples] = outSlot[1][i]*decay_factor;
+    for (int i = delay_samples; i< buffer_size; i++){
+        outSlot[i] = inSlot[i-delay_samples] + gain * outSlot[i-delay_samples];
     }
 };
 
-void all_pass_filter(float ** inSlot, 
-                 float ** outSlot,
-                 int buffer_size,
-                 int sample_rate)
-{
-    // ToDo: understand calculation of decay factor and samples
-    for (int i = 0; i< buffer_size; i++){
-            outSlot[0][i] = inSlot[0][i];
-            outSlot[1][i] = inSlot[1][i];
-
-            if (i - delay_samples >= 0 ) {
-                outSlot[0][i] += -decay_factor * outSlot[i-delay_samples];
-                outSlot[1][i] += -decay_factor * outSlot[i-delay_samples];
-            }
-
-            if (i - delay_samples >= 1)
-        }
-    
+void normalise_and_smooth(float * outSlot, int buffer_size){
+    // Find maximum of outSlot, scale all values by this.
+    max_absolute_pointer = std::max_element(outSlot[0], outSlot[buffer_size], [](float a, float b) {
+        return std::abs(a) < std::abs(b);
+    });
+    float value = outSlot[0];
+    for (int i=0; i<buffer_size; i++){
+        // Smooth by only adding the variation between values, and scaling by max.
+        outSlot[i] = (value + (outSlot[i]-value))/ (*max_absolute_pointer);
+        value = outSlot[i];
+    }
 }
+
+void all_pass_filter(float * inSlot, 
+                 float * outSlot,
+                 int buffer_size,
+                 int sample_rate,
+                 float delay_ms,
+                 float gain)
+{
+    gain_squared = 1 - gain*gain;
+    intermediatSlot = new float[buffer_size];
+    for (int i = 0; i< buffer_size; i++){
+        if (i < delay_samples) {
+            // Delayed samples not yet looped around, out = -g * in
+            outSlot[i] = -gain * inSlot[i];
+            intermediatSlot[i] = 0;
+        } else {
+            intermediatSlot[i] = inSlot[i-delay_samples] + gain*intermediatSlot[i-delay_samples];
+            outSlot[i] = -gain * inSlot[i] + gain_squared * intermediatSlot[i];
+        }
+    }
+    normalise_and_smooth(outSlot, buffer_size);
+}
+
